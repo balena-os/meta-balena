3916ca850dfa2761d68817380ceda899592c2e03
Praneeth <lifeeth@resin.io>
2014-06-03 11:43:20 +0000

    Over-riding psplash with custom one. Time to override everything else :)

---
+++ recipes-core/psplash/make-image-header.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Install libgdk-pixbuf2.0-dev for the following to work.
+
+set -e
+
+imageh=`basename $1 .png`-img.h
+name="${2}_IMG"
+gdk-pixbuf-csource --macros $1 > $imageh.tmp
+sed -e "s/MY_PIXBUF/${name}/g" -e "s/guint8/uint8/g" $imageh.tmp > $imageh && rm $imageh.tmp
---
+++ recipes-core/psplash/psplash_git.bbappend
@@ -0,0 +1,2 @@
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+SPLASH_IMAGES += "file://psplash-resin-img.h;outsuffix=resin"
