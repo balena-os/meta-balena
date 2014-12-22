9b096cc88eec8db3489bcc59229f5d738b4a01e9
Praneeth Bodduluri <lifeeth@resin.io>
2014-12-22 21:52:50 +0100

    EMMC and MMC boots seperated properly - Functioning provisioning without supervisor

--- meta-resin-beaglebone/recipes-images/beaglebone-flasher/files/init-bbb-flasher.sh
+++ meta-resin-beaglebone/recipes-images/beaglebone-flasher/files/init-bbb-flasher.sh
@@ -32,15 +32,16 @@ parted -s ${SDCARD} unit KiB mkpart primary ext4 $(expr ${BOOT_SPACE_ALIGNED} \+
 parted -s ${SDCARD} unit KiB mkpart primary linux-swap $(expr ${BOOT_SPACE_ALIGNED} \+ ${IMAGE_ROOTFS_ALIGNMENT} \+ ${ROOTFS_SIZE}) $(expr ${BOOT_SPACE_ALIGNED} \+ ${IMAGE_ROOTFS_ALIGNMENT} \+ ${ROOTFS_SIZE} \+ ${SWAP_SIZE})
 
 # Create docker data partition with the rest of the space.
-parted -s ${SDCARD} unit KiB mkpart primary btrfs $(expr ${BOOT_SPACE_ALIGNED} \+ ${IMAGE_ROOTFS_ALIGNMENT} \+ ${ROOTFS_SIZE} \+ ${SWAP_SIZE}) 100%
+parted -s ${SDCARD} unit KiB mkpart primary ext4 $(expr ${BOOT_SPACE_ALIGNED} \+ ${IMAGE_ROOTFS_ALIGNMENT} \+ ${ROOTFS_SIZE} \+ ${SWAP_SIZE}) 100%
 
 parted ${SDCARD} print
 
 partprobe
 
-mkfs.vfat -F 16 /dev/mmcblk1p1 
-mkfs.ext4 /dev/mmcblk1p2 
-mkswap /dev/mmcblk1p3
+mkfs.vfat -F 16 /dev/mmcblk1p1  #BOOT
+mkfs.ext4 /dev/mmcblk1p2  #ROOT
+mkswap /dev/mmcblk1p3 #SWAP
+mkfs.btrfs -f /dev/mmcblk1p4 #DATA
 
 echo 1 > /sys/class/leds/beaglebone:green:mmc0/brightness
 
@@ -54,13 +55,19 @@ mount /dev/mmcblk1p1 /tmp/new_boot
 
 cp /tmp/boot/MLO /tmp/new_boot/ # This needs to go first into the partition.
 cp /tmp/boot/u-boot-emmc.img /tmp/new_boot/u-boot.img # Copy the emmc specific u-boot to emmc
+cp /tmp/boot/image-version-info /tmp/new_boot/image-version-info # Copy the uboot build info
 rsync -a --numeric-ids --exclude='/tmp/*' --exclude='/dev/*' --exclude='/proc/*' --exclude='/sys/*' --exclude='/var/volatile/*' --exclude='/run/*' / /tmp/new_root/ # rsync the rest of the files.
 
-export ROOTUUID=`blkid -s PARTUUID -o value /dev/mmcblk1p1`
-export SWAPUUID=`blkid -s PARTUUID -o value /dev/mmcblk1p2`
-export BTRFSUUID=`blkid -s PARTUUID -o value /dev/mmcblk1p3`
+export BOOTUUID=`blkid -s UUID -o value /dev/mmcblk1p1`
+export ROOTPARTUUID=`blkid -s PARTUUID -o value /dev/mmcblk1p2`
+export SWAPUUID=`blkid -s UUID -o value /dev/mmcblk1p3`
+export BTRFSUUID=`blkid -s UUID -o value /dev/mmcblk1p4`
 
-echo "setenv mmcroot PARTUUID=${ROOTUUID} rw; run mmcboot;" >> /tmp/new_boot/uEnv.txt
+echo "uenvcmd=setenv mmcroot PARTUUID=${ROOTPARTUUID} rw;" > /tmp/new_boot/uEnv.txt
+echo 1 > /tmp/new_boot/REMOVE_TO_REPROVISION_${BOOTUUID}
+echo 1 > /tmp/boot/REMOVE_TO_REPROVISION_${BOOTUUID}
+# The following command ensures reboot to the new env immediately after provisioning and at the same time it ensures that the same SD card can be used to provision multiple Beagblebones in one shot.
+echo "uenvcmd=if load mmc 0 0x82000000 REMOVE_TO_REPROVISION_${BOOTUUID}; then if load mmc 1 0x82000000 REMOVE_TO_REPROVISION_${BOOTUUID};then setenv mmcdev 1; run mmcboot; fi; fi;" > /tmp/boot/uEnv.txt
 
 echo 1 > /sys/class/leds/beaglebone:green:usr2/brightness
 
