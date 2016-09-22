d2f88395920dc6302d4f138e0f620ba5f7840a81
Andrei Gherzan <andrei@resin.io>
2016-09-22 17:23:57 +0200

    run-resinhup.sh: Run blkid after setting PATH (source)
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- meta-resin-common/recipes-support/resinhup/resinhup/run-resinhup.sh
+++ meta-resin-common/recipes-support/resinhup/resinhup/run-resinhup.sh
@@ -8,9 +8,11 @@ LOGFILE=/tmp/`basename "$0"`.log
 LOG=yes
 ONLY_SUPERVISOR=no
 NOREBOOT=no
-BOOT_MOUNTPOINT="$(grep $(blkid | grep resin-boot | cut -d ":" -f 1) /proc/mounts | cut -d ' ' -f 2)"
 
+# Don't run anything before this source as it sets PATH here
 source /etc/profile
+
+BOOT_MOUNTPOINT="$(grep $(blkid | grep resin-boot | cut -d ":" -f 1) /proc/mounts | cut -d ' ' -f 2)"
 
 # Help function
 function help {
