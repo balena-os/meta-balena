MagnusTiberius/meta-resin

healthdog: Add new recipe
Add a package for the healthdog health monitoring watchdog.

https://github.com/resin-os/healthdog-rs

Change-type: patch
Changelog-entry: Add healthdog recipe
Signed-off-by: Will Newton <willn@resin.io>

meta-resin-common/recipes-core/healthdog/healthdog_0.1.0.bb
@@ -1,0 +1,54 @@
+# Auto-Generated by cargo-bitbake 0.3.7
+#
+inherit cargo
+
+# If this is git based prefer versioned ones if they exist
+# DEFAULT_PREFERENCE = "-1"
+
+# how to get healthdog could be as easy as but default to a git checkout:
+# SRC_URI += "crate://crates.io/healthdog/0.1.0"
+SRC_URI += "git://git@github.com/resin-os/healthdog-rs.git;protocol=ssh"
+SRCREV = "d846171fdb12d14099e94bd42f933361023bf308"
+S = "${WORKDIR}/git"
+CARGO_SRC_DIR=""
+PV_append = ".AUTOINC+d846171fdb"
+
+# please note if you have entries that do not begin with crate://
+# you must change them to how that package can be fetched
+SRC_URI += " \
+crate://crates.io/bitflags/0.7.0 \
+crate://crates.io/cfg-if/0.1.1 \
+crate://crates.io/errno/0.2.3 \
+crate://crates.io/exec/0.3.1 \
+crate://crates.io/getopts/0.2.14 \
+crate://crates.io/kernel32-sys/0.2.2 \
+crate://crates.io/libc/0.2.24 \
+crate://crates.io/libsystemd-sys/0.1.0 \
+crate://crates.io/log/0.3.8 \
+crate://crates.io/mbox/0.3.1 \
+crate://crates.io/nix/0.8.1 \
+crate://crates.io/pkg-config/0.3.9 \
+crate://crates.io/rustc_version/0.2.1 \
+crate://crates.io/semver-parser/0.7.0 \
+crate://crates.io/semver/0.6.0 \
+crate://crates.io/systemd/0.1.0 \
+crate://crates.io/utf8-cstr/0.1.6 \
+crate://crates.io/void/1.0.2 \
+crate://crates.io/winapi-build/0.1.1 \
+crate://crates.io/winapi/0.2.8 \
+"
+
+LIC_FILES_CHKSUM=" \
+file://LICENSE;md5=3bfd34238ccc26128aef96796a8bbf97 \
+"
+
+SUMMARY = "Helper program that connects external periodic heathchecks with systemd's watchdog support"
+HOMEPAGE = "https://github.com/resin-os/healthdog-rs.git"
+LICENSE = "Apache-2.0"
+
+DEPENDS += "systemd"
+
+# includes this file if it exists but does not fail
+# this is useful for anything you may want to override from
+# what cargo-bitbake generates.
+include healthdog.inc

balena-os/meta-balena

meta-resin-common/recipes-core/healthdog/healthdog_0.1.0.bb
@@ -1,0 +1,52 @@
+# Auto-Generated by cargo-bitbake 0.3.7
+#
+inherit cargo
+
+# If this is git based prefer versioned ones if they exist
+# DEFAULT_PREFERENCE = "-1"
+
+# how to get healthdog could be as easy as but default to a git checkout:
+# SRC_URI += "crate://crates.io/healthdog/0.1.0"
+SRC_URI += "git://git@github.com/resin-os/healthdog-rs.git;protocol=ssh"
+SRCREV = "a10155844f251d00ec49348e58b2846803386085"
+S = "${WORKDIR}/git"
+CARGO_SRC_DIR=""
+PV_append = ".AUTOINC+a10155844f"
+
+# please note if you have entries that do not begin with crate://
+# you must change them to how that package can be fetched
+SRC_URI += " \
+	crate://crates.io/bitflags/0.7.0 \
+	crate://crates.io/cfg-if/0.1.1 \
+	crate://crates.io/errno/0.2.3 \
+	crate://crates.io/exec/0.3.1 \
+	crate://crates.io/getopts/0.2.14 \
+	crate://crates.io/kernel32-sys/0.2.2 \
+	crate://crates.io/libc/0.2.24 \
+	crate://crates.io/libsystemd-sys/0.1.0 \
+	crate://crates.io/log/0.3.8 \
+	crate://crates.io/mbox/0.3.1 \
+	crate://crates.io/nix/0.8.1 \
+	crate://crates.io/pkg-config/0.3.9 \
+	crate://crates.io/rustc_version/0.2.1 \
+	crate://crates.io/semver-parser/0.7.0 \
+	crate://crates.io/semver/0.6.0 \
+	crate://crates.io/systemd/0.1.0 \
+	crate://crates.io/utf8-cstr/0.1.6 \
+	crate://crates.io/void/1.0.2 \
+	crate://crates.io/winapi-build/0.1.1 \
+	crate://crates.io/winapi/0.2.8 \
+	"
+
+LIC_FILES_CHKSUM="file://LICENSE;md5=3bfd34238ccc26128aef96796a8bbf97"
+
+SUMMARY = "Helper program that connects external periodic healthchecks with systemd's watchdog support"
+HOMEPAGE = "https://github.com/resin-os/healthdog-rs.git"
+LICENSE = "Apache-2.0"
+
+DEPENDS += "systemd"
+
+# includes this file if it exists but does not fail
+# this is useful for anything you may want to override from
+# what cargo-bitbake generates.
+include healthdog.inc