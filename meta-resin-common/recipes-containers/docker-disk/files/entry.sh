c7ebfaa644a79ba5ba9d45411e02356b4f1068c4
Andrei Gherzan <andrei@resin.io>
2018-02-02 18:00:38 +0000

    docker-disk: Various improvements
    Currently we populate the docker disk partition by using a loop device and the
    build outputs a fs file owned by root. We can do better:
    1. stop using loop devices
    2. have compile populate a directory which includes the content of the docker
    partition
    3. add a task which creates the filesystem out of the directory populated
    above (after compile before install)
    4. create the filesystem with fakeroot (chowning to root) so the final fs will
    have content owned by root
    
    This work was part of the work done for #114 which was aiming to run the
    docker container which creates the docker partition unprivileged. Currently
    that is not possible due to the fact that we run docker in docker which
    requires elevated permissions.
    
    This patch fixes as well a racing issue where multiple builds might have
    collied when creating the looper imager (was renamed as well to be clearer what
    it is). Now we create images that are unique per build and we cleanup
    afterwards the image.
    
    Change-type: patch
    Change-log-entry: Various build improvements for docker-disk recipe
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- meta-resin-common/recipes-containers/docker-disk/files/entry.sh
+++ meta-resin-common/recipes-containers/docker-disk/files/entry.sh
@@ -4,32 +4,25 @@ set -o errexit
 set -o nounset
 
 DOCKER_TIMEOUT=20 # Wait 20 seconds for docker to start
+DATA_VOLUME=/resin-data
 
-# Default values
-PARTITION_SIZE=${PARTITION_SIZE:=1024}
-
-# Create sparse file to hold ext4 resin-data partition
-dd if=/dev/zero of=/export/resin-data.img bs=1M count=0 seek=$PARTITION_SIZE
-# now partition the newly created file to ext4
-mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 -i 8192 -F /export/resin-data.img
-
-# Setup the loop device with the disk image
-mkdir /resin-data
-mount -o loop /export/resin-data.img /resin-data
+# Create user
+echo "[INFO] Creating and setting $USER_ID:$USER_GID."
+groupadd -g "$USER_GID" docker-disk-group
+useradd -u "$USER_ID" -g "$USER_GID" -p "" docker-disk-user
 
 # Create the directory structures we use for Resin
-mkdir -p /resin-data/docker
-mkdir -p /resin-data/resin-data
+mkdir -p $DATA_VOLUME/{docker,resin-data}
 
 # Start docker with the created image
 echo "Starting docker daemon with $BALENA_STORAGE storage driver."
-docker daemon -g /resin-data/docker -s $BALENA_STORAGE &
+docker daemon -g $DATA_VOLUME/docker -s "$BALENA_STORAGE" &
 echo "Waiting for docker to become ready.."
-STARTTIME=$(date +%s)
-ENDTIME=$(date +%s)
+STARTTIME="$(date +%s)"
+ENDTIME="$(date +%s)"
 while [ ! -S /var/run/docker.sock ]
 do
-    if [ $(($ENDTIME - $STARTTIME)) -le $DOCKER_TIMEOUT ]; then
+    if [ $((ENDTIME - STARTTIME)) -le $DOCKER_TIMEOUT ]; then
         sleep 1
         ENDTIME=$(date +%s)
     else
@@ -41,10 +34,11 @@ echo "Docker started."
 
 if [ -n "${TARGET_REPOSITORY}" ] && [ -n "${TARGET_TAG}" ]; then
     echo "Pulling ${TARGET_REPOSITORY}:${TARGET_TAG}..."
-    docker pull $TARGET_REPOSITORY:$TARGET_TAG
+	docker pull "${TARGET_REPOSITORY}:${TARGET_TAG}"
 fi
 
 echo "Stopping docker..."
-kill -TERM $(cat /var/run/docker.pid) && wait $(cat /var/run/docker.pid) && umount /resin-data
+kill -TERM "$(cat /var/run/docker.pid)" && wait "$(cat /var/run/docker.pid)"
 
-echo "Docker export successful."
+# Make all files owned by the build system
+chown -R "$USER_ID:$USER_GID" "$DATA_VOLUME"
