balena-os/meta-balena

commit 63baa421ada42aa6090e67e60324bd90ebbab873

kernel-modules-headers: Update version to v0.0.11
This version uses hostcc (last argument) for modules_prepare as well
(not only for for compiling the scripts). On newer kernel versions,
modules_prepare needs more than just the CC but also sysroot, ld flags
etc. Add all these so tools like extract_cert can be compiled during
modules_prepare.

 Fixes #1143

Change-type: patch
Changelog-entry: Update kernel-modules-headers to v0.0.11
Signed-off-by: Andrei Gherzan <andrei@resin.io>

meta-resin-common/recipes-devtools/kernel-modules-headers/kernel-modules-headers.bb
@@ -11,22 +11,25 @@ DEPENDS = " \
     virtual/kernel \
     bc-native \
     openssl \
+    openssl-native \
     "
 
 SRC_URI = "git://github.com/resin-os/module-headers.git;protocol=https"
 
-SRCREV = "v0.0.10"
+SRCREV = "v0.0.11"
 
 S = "${WORKDIR}/git"
 B = "${WORKDIR}"
 
 inherit deploy kernel-arch
 
+HOSTCC = "${BUILD_CC} ${BUILD_CFLAGS} ${BUILD_LDFLAGS}"
+
 do_configure[noexec] = "1"
 
 do_compile() {
     mkdir -p kernel_modules_headers
-    ${S}/gen_mod_headers ./kernel_modules_headers ${STAGING_KERNEL_DIR} ${DEPLOY_DIR_IMAGE} ${ARCH} ${TARGET_PREFIX} "${CC}"
+    ${S}/gen_mod_headers ./kernel_modules_headers ${STAGING_KERNEL_DIR} ${DEPLOY_DIR_IMAGE} ${ARCH} ${TARGET_PREFIX} "${HOSTCC}"
     tar -czf kernel_modules_headers.tar.gz kernel_modules_headers
     rm -rf kernel_modules_headers
 }

commit 706829bc46c593048909686a67faef064adf6232

kernel-modules-headers: Update to v0.0.12
63baa421 introduced a bug where the tools in the header tarballs were
compiled with the incorrect HOSTCC parameter. As a result the tools
inside the tarball were compiled for x86-64 instead of the native
device architecture. And they had the incorrect ld interpreter path
as well.

Update to v0.0.12 to fix that.
This release also adds objtool support needed by the intel nuc image.

Fixes #1302
Fixes #1303

Change-type: minor
Changelog-entry: Fix kernel module header generation
Signed-off-by: Zubair Lutfullah Kakakhel <zubair@balena.io>

meta-resin-common/recipes-devtools/kernel-modules-headers/kernel-modules-headers.bb
@@ -12,11 +12,15 @@ DEPENDS = " \
     bc-native \
     openssl \
     openssl-native \
+    util-linux-native \
+    elfutils-native \
+    util-linux \
+    elfutils \
     "
 
 SRC_URI = "git://github.com/resin-os/module-headers.git;protocol=https"
 
-SRCREV = "v0.0.11"
+SRCREV = "v0.0.12"
 
 S = "${WORKDIR}/git"
 B = "${WORKDIR}"
@@ -29,7 +33,7 @@ do_configure[noexec] = "1"
 
 do_compile() {
     mkdir -p kernel_modules_headers
-    ${S}/gen_mod_headers ./kernel_modules_headers ${STAGING_KERNEL_DIR} ${DEPLOY_DIR_IMAGE} ${ARCH} ${TARGET_PREFIX} "${HOSTCC}"
+    ${S}/gen_mod_headers ./kernel_modules_headers ${STAGING_KERNEL_DIR} ${DEPLOY_DIR_IMAGE} ${ARCH} ${TARGET_PREFIX} "${CC}" "${HOSTCC}"
     tar -czf kernel_modules_headers.tar.gz kernel_modules_headers
     rm -rf kernel_modules_headers
 }