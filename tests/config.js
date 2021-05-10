83bcc416ca52fcb71404f980ad932f150503fd22
Kyle Harding <kyle@balena.io>
2021-05-10 13:28:09 +0000

    tests: Use pre-defined Leviathan config for Jenkins jobs
    Include both os and hup suites for now, and new suites
    can be added to config.js as needed.
    
    The deviceType will be substituted by Jenkins at
    build time, before the suites are run.
    
    Use BALENA_CLOUD_ORG and BALENA_CLOUD_API_KEY
    
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ tests/config.js
@@ -0,0 +1,37 @@
+module.exports = [{
+	deviceType: `raspberrypi3`,
+	suite: `${__dirname}/../suites/os`,
+	config: {
+		networkWired: false,
+		networkWireless: true,
+		downloadType: 'local',
+		interactiveTests: false, // redundant
+		balenaApiKey: process.env.BALENA_CLOUD_API_KEY,
+		balenaApiUrl: 'balena-cloud.com',
+		organization: process.env.BALENA_CLOUD_ORG
+	},
+	image: `${__dirname}/balena.img.gz`,
+	workers: {
+		balenaApplication: process.env.BALENA_CLOUD_APP_NAME,
+		apiKey: process.env.BALENA_CLOUD_API_KEY,
+	},
+},
+{
+	deviceType: `raspberrypi3`,
+	suite: `${__dirname}/../suites/hup`,
+	config: {
+		networkWired: false,
+		networkWireless: true,
+		downloadType: 'gunzip',
+		downloadVersion: 'latest',
+		interactiveTests: false, // redundant
+		balenaApiKey: process.env.BALENA_CLOUD_API_KEY,
+		balenaApiUrl: 'balena-cloud.com',
+		organization: process.env.BALENA_CLOUD_ORG
+	},
+	image: `${__dirname}/balena-image.docker`,
+	workers: {
+		balenaApplication: process.env.BALENA_CLOUD_APP_NAME,
+		apiKey: process.env.BALENA_CLOUD_API_KEY,
+	},
+}];
