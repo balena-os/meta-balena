a07bb440b0cad6c5948362fdfbd24fb14b8b5ad9
Robert Günzler <r@gnzler.io>
2021-06-17 12:14:13 +0000

    DONOTMERGE disable os test suite
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- tests/suites/config.js
+++ tests/suites/config.js
@@ -1,20 +1,21 @@
-module.exports = [{
-	deviceType: `raspberrypi3`,
-	suite: `${__dirname}/../suites/os`,
-	config: {
-		networkWired: false,
-		networkWireless: true,
-		interactiveTests: false, // redundant
-		balenaApiKey: process.env.BALENA_CLOUD_API_KEY,
-		balenaApiUrl: 'balena-cloud.com',
-		organization: process.env.BALENA_CLOUD_ORG
-	},
-	image: `${__dirname}/balena.img.gz`,
-	workers: {
-		balenaApplication: process.env.BALENA_CLOUD_APP_NAME,
-		apiKey: process.env.BALENA_CLOUD_API_KEY,
-	},
-},
+module.exports = [
+// {
+// 	deviceType: `raspberrypi3`,
+// 	suite: `${__dirname}/../suites/os`,
+// 	config: {
+// 		networkWired: false,
+// 		networkWireless: true,
+// 		interactiveTests: false, // redundant
+// 		balenaApiKey: process.env.BALENA_CLOUD_API_KEY,
+// 		balenaApiUrl: 'balena-cloud.com',
+// 		organization: process.env.BALENA_CLOUD_ORG
+// 	},
+// 	image: `${__dirname}/balena.img.gz`,
+// 	workers: {
+// 		balenaApplication: process.env.BALENA_CLOUD_APP_NAME,
+// 		apiKey: process.env.BALENA_CLOUD_API_KEY,
+// 	},
+// },
 {
 	deviceType: `raspberrypi3`,
 	suite: `${__dirname}/../suites/hup`,
