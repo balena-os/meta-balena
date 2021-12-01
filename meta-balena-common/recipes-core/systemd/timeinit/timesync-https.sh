2bb18707557979ed6466f45602de4ecf5b386c57
Mark Corbin <mark@balena.io>
2021-12-01 15:02:17 +0000

    systemd/timeinit: add HTTPS time synchronisation service
    Add a new timesync-https systemd service to synchronise the system
    time at boot using an HTTPS header. The service uses curl to request
    an HTTPS header from either $API_ENDPOINT/connectivity-check (default)
    or the URL defined by the os.network.connectivity.uri field in
    config.json. The URL used *must* return HTTP code 204 (No Content)
    in response to a request so that we can determine that we have full
    network connectivity and are not operating behind a captive portal.
    
    The date field returned by a valid header is used to set the current
    system time. The date/time derived from the header is assumed to be a
    reasonable source of 'truth' such that it can be used to adjust the
    system time both backwards and forwards. This will compensate for any
    erroneous timestamps saved via fake-hwclock or any invalid data
    read from an RTC.
    
    The service will exit when a valid response has been received. Poll
    attempts will be made at an increasing interval starting at 2s and
    doubling up to a maximum of 64s. Polling will continue at the maximum
    interval until a valid response has been received.
    
    This service will provide initial time synchronisation for devices
    where NTP ports have been blocked. For devices where NTP access is
    available it should ensure that any system 'time jump' is only a few
    seconds when NTP synchronisation is eventually achieved. It also
    allows other services to start with a reasonably accurate time
    without having to wait for the NTP synchronisation process to
    complete.
    
    Services that are ordered after the new time-sync-https-wait target
    can be sure that full network connectivity has been achieved and that
    time has been synchronised with an accuracy of a few seconds.
    
    Change-type: minor
    Connects-to: #1337 #1776 #2044 #2139
    Signed-off-by: Mark Corbin <mark@balena.io>

---
+++ meta-balena-common/recipes-core/systemd/timeinit/timesync-https.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+#
+# Copyright 2021 Balena Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+. /usr/libexec/os-helpers-logging
+. /usr/libexec/os-helpers-time
+
+. /usr/sbin/balena-config-vars
+
+# Expected HTTP response code. Used to determine that we are not
+# behind a captive portal.
+EXPECTED_SERVER_CODE=204
+# Initial delay in seconds between poll attempts.
+INITIAL_HTTPS_POLL_DELAY=2
+# Maximum delay in seconds between poll attempts.
+MAX_HTTPS_POLL_DELAY=64
+# Timeout for curl command in seconds.
+# Note that curl does not apply this timeout to DNS lookups.
+CURL_TIMEOUT=5
+# Don't bother updating or reporting errors for small differences.
+TIME_DIFF_THRESHOLD=2
+
+# Poll HTTPS server for time string.
+info "Starting HTTPS time synchronisation."
+
+HTTPS_POLL_DELAY=$INITIAL_HTTPS_POLL_DELAY
+
+# In theory the maximum duration of each poll delay is given by:
+#     (HTTPS_POLL_DELAY + CURL_TIMEOUT) seconds.
+# Note that this period can be extended as curl DNS lookup timeouts do
+# not obey the -m (--max-time) parameter.
+
+if [ -z "$OS_NET_CONN_URI" ]; then
+	warn "Connectivity URL not defined - skipping HTTPS synchronisation."
+	exit 0
+fi
+
+while [ true  ]; do
+	SYS_TIME=$(get_system_time_as_timestamp)
+	readarray -t https_header <<<$(curl -m5 -k -I -s $OS_NET_CONN_URI | sed 's/\r$//'  | awk '/HTTP/{printf $2"\n"} /[Dd]ate/{print $2, $3, $4, $5, $6, $7"\n"}')
+	SERVER_CODE=${https_header[0]}
+	SERVER_TIME_STRING=${https_header[1]}
+	if [ "$SERVER_CODE" = "$EXPECTED_SERVER_CODE" ]; then
+		if [ ! -z "$SERVER_TIME_STRING" ]; then
+			SERVER_TIME=$(get_server_time_as_timestamp "$SERVER_TIME_STRING")
+			TIME_DIFF=$(get_abs_time_diff_from_timestamps "$SYS_TIME" "$SERVER_TIME")
+			if [ "$TIME_DIFF" -gt "$TIME_DIFF_THRESHOLD" ]; then
+				$(set_system_time_from_timestamp "$SERVER_TIME")
+				if [ "$SYS_TIME" -gt "$SERVER_TIME" ]; then
+					warn "HTTPS header time is in the past."
+					warn "Check time sources if this issue persists."
+				fi
+				info "Time synchronised via HTTPS."
+				info "Old time: $(get_display_time_from_timestamp "$SYS_TIME")"
+				info "New time: $(get_display_time_from_timestamp "$SERVER_TIME")"
+				exit 0
+			else
+				info "System time is already synchronised."
+				exit 0
+			fi
+		fi
+	fi
+	sleep $HTTPS_POLL_DELAY
+
+	if [ "$HTTPS_POLL_DELAY" -lt "$MAX_HTTPS_POLL_DELAY" ]; then
+		HTTPS_POLL_DELAY=$(($HTTPS_POLL_DELAY * 2))
+	fi
+done
