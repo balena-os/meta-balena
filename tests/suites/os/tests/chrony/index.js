e3dbd8cbbefcda1bd1f34732586e6ed664941b9c
Joseph Kogut <joseph@balena.io>
2021-12-15 17:10:31 -0800

    os: tests: optimize chrony tests
    Promisify commands run in chrony tests, reducing the time taken.
    
    - ok 1 - chrony tests # time=32600.373ms
    + ok 1 - chrony tests # time=21252.641ms
    
    Change-type: patch
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- tests/suites/os/tests/chrony/index.js
+++ tests/suites/os/tests/chrony/index.js
@@ -5,63 +5,43 @@ module.exports = {
 			title: 'Chronyd service',
 			run: async function(test) {
 				test.comment(`checking for chronyd service...`);
-				let result = '';
-				await this.context.get().utils.waitUntil(async () => {
-					result = await this.context
-						.get()
-						.worker.executeCommandInHostOS(
-							`systemctl is-active chronyd.service`,
-							this.context.get().link,
-						);
-					return result === 'active';
-				}, false);
-				result = await this.context
-					.get()
-					.worker.executeCommandInHostOS(
-						'systemctl status chronyd | grep running',
+				let p = this.context.get().utils.waitUntil(async () => {
+					return this.context.get().worker.executeCommandInHostOS(
+						`systemctl is-active chronyd.service`,
 						this.context.get().link,
-					);
-				test.is(result !== '', true, 'Chronyd service should be running');
+					).then((result) => {
+						return result === 'active';
+					});
+				}, false, 60, 1000);
+
+				return test.resolves(p, 'Chronyd service should be running');
 			},
 		},
 		{
 			title: 'Sync test',
 			run: async function(test) {
-				let result = '';
-				await this.context.get().utils.waitUntil(async () => {
+				let p = this.context.get().utils.waitUntil(async () => {
 					test.comment('checking system clock synchronized...');
-					result = await this.context
-						.get()
-						.worker.executeCommandInHostOS(
+					return this.context.get().worker.executeCommandInHostOS(
 							'timedatectl | grep System',
 							this.context.get().link,
-						);
-					return result === 'System clock synchronized: yes';
-				});
-				result = await this.context
-					.get()
-					.worker.executeCommandInHostOS(
-						'timedatectl | grep System',
-						this.context.get().link,
-					);
-				test.is(
-					result,
-					'System clock synchronized: yes',
-					'System clock should be synchronized',
-				);
+						).then((result) => {
+							return result === 'System clock synchronized: yes';
+						});
+				}, false, 60, 1000);
+
+				return test.resolves(p, 'System clock should be synchronized');
 			},
 		},
 		{
 			title: 'Source test',
 			run: async function(test) {
-				let result = '';
-				result = await this.context
-					.get()
-					.worker.executeCommandInHostOS(
-						`chronyc sources -n | fgrep '^*'`,
-						this.context.get().link,
-					);
-				test.is(result !== '', true, 'Should see ^* next to chrony source');
+				return this.context.get().worker.executeCommandInHostOS(
+					`chronyc sources -n | fgrep '^*'`,
+					this.context.get().link,
+				).then((result) => {
+					test.is(result !== '', true, 'Should see ^* next to chrony source');
+				});
 			},
 		},
 	],
