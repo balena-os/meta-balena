88a1701a5f67812461c0b41f5fad7a47afc4a7b5
Joseph Kogut <joseph@balena.io>
2022-02-02 00:32:24 +0000

    tests: os: connectivity: remove context references
    Change-type: patch
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- tests/suites/os/tests/connectivity/index.js
+++ tests/suites/os/tests/connectivity/index.js
@@ -42,23 +42,19 @@ module.exports = {
 					},
 					run: async function(test) {
 						let testIface = adaptor === 'wireless' ? 'wifi' : 'ethernet';
-						const iface = await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								`nmcli d  | grep ' ${testIface} ' | grep connected | awk '{print $1}'`,
-								this.context.get().link,
-							);
+						const iface = await this.worker.executeCommandInHostOS(
+							`nmcli d  | grep ' ${testIface} ' | grep connected | awk '{print $1}'`,
+							this.link,
+						);
 
 						if (iface === '') {
 							throw new Error(`No ${testIface} interface found.`);
 						}
 
-						let ping = await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								`ping -c 10 -i 0.002 -I ${iface} ${URL_TEST}`,
-								this.context.get().link,
-							);
+						let ping = await this.worker.executeCommandInHostOS(
+							`ping -c 10 -i 0.002 -I ${iface} ${URL_TEST}`,
+							this.link,
+						);
 
 						test.ok(
 							ping.includes('10 packets transmitted, 10 packets received'),
@@ -75,92 +71,76 @@ module.exports = {
 					title: `${proxy.charAt(0).toUpperCase()}${proxy.slice(1)} test`,
 					run: async function(test) {
 						let getProxyContainerID = async() => {
-							return this.context
-								.get()
-								.worker.executeCommandInHostOS(
-									['balena', 'ps', '-qf', 'name=proxy'].join(' '),
-									this.context.get().link
-								);
+							return this.worker.executeCommandInHostOS(
+								['balena', 'ps', '-qf', 'name=proxy'].join(' '),
+								this.link
+							);
 						};
 
-						const ip = await this.context.get()
-							.worker.ip(this.context.get().link);
+						const ip = await this.worker.ip(this.link);
 
 						// Ensure we only push and run the proxy container once
 						if (!await getProxyContainerID()) {
 							test.comment('Running proxy in container');
-							const state = await this.context.get()
-								.worker.pushContainerToDUT(ip, __dirname, 'proxy');
+							const state = await this.worker.pushContainerToDUT(
+								ip, __dirname, 'proxy'
+							);
 							test.comment(state);
 						}
 
-						await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								'mkdir -p /mnt/boot/system-proxy',
-								this.context.get().link,
-							);
+						await this.worker.executeCommandInHostOS(
+							'mkdir -p /mnt/boot/system-proxy', this.link,
+						);
 
 						test.comment(`Creating redsocks.conf for ${proxy}...`);
-						await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								'printf "' +
-									'base { \n' +
-									'log_debug = off; \n' +
-									'log_info = on; \n' +
-									'log = stderr; \n' +
-									'daemon = off; \n' +
-									'redirector = iptables; \n' +
-									'} \n' +
-									'redsocks { \n' +
-									`type = ${proxy}; \n` +
-									`ip = 127.0.0.1; \n` +
-									`port = 8123; \n` +
-									'local_ip = 127.0.0.1; \n' +
-									'local_port = 12345; \n' +
-									'} \n" > /mnt/boot/system-proxy/redsocks.conf',
-								this.context.get().link,
-							);
+						await this.worker.executeCommandInHostOS(
+							'printf "' +
+								'base { \n' +
+								'log_debug = off; \n' +
+								'log_info = on; \n' +
+								'log = stderr; \n' +
+								'daemon = off; \n' +
+								'redirector = iptables; \n' +
+								'} \n' +
+								'redsocks { \n' +
+								`type = ${proxy}; \n` +
+								`ip = 127.0.0.1; \n` +
+								`port = 8123; \n` +
+								'local_ip = 127.0.0.1; \n' +
+								'local_port = 12345; \n' +
+								'} \n" > /mnt/boot/system-proxy/redsocks.conf',
+							this.link,
+						);
 
 						// the supervisor would do this if proxy config were set via the supervisor sdk
 						// https://www.balena.io/docs/reference/OS/network/2.x/#connecting-behind-a-proxy
 						test.comment(`Manually restarting services...`);
-						await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								'systemctl restart balena-proxy-config.service redsocks.service',
-								this.context.get().link,
-							);
+						await this.worker.executeCommandInHostOS(
+							'systemctl restart balena-proxy-config.service redsocks.service',
+							this.link,
+						);
 
 						test.is(
-							await this.context
-								.get()
-								.worker.executeCommandInHostOS(
-									'systemctl is-active redsocks.service',
-									this.context.get().link,
-								),
+							await this.worker.executeCommandInHostOS(
+								'systemctl is-active redsocks.service',
+								this.link,
+							),
 							'active',
 							'Redsocks proxy service should be active',
 						);
 
-						await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								`curl -I https://${URL_TEST}`,
-								this.context.get().link,
-							);
+						await this.worker.executeCommandInHostOS(
+							`curl -I https://${URL_TEST}`, this.link,
+						);
 
 						test.comment('Getting proxy container logs...');
-						const proxyLog = await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								['balena',
-									'logs',
-									await getProxyContainerID(),
-									'|', 'tail', '-n1'].join(' '),
-								this.context.get().link,
-							);
+						const proxyLog = await this.worker.executeCommandInHostOS(
+							['balena',
+								'logs',
+								await getProxyContainerID(),
+								'|', 'tail', '-n1'].join(' '),
+							this.link,
+						);
 
 						const pattern = {
 							'socks5': new RegExp(/\[socks5\] 127\.0\.0\.1:[0-9]* <->/),
@@ -175,20 +155,15 @@ module.exports = {
 						);
 
 						test.comment(`Removing redsocks.conf...`);
-						await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								'rm -rf /mnt/boot/system-proxy',
-								this.context.get().link,
-							);
+						await this.worker.executeCommandInHostOS(
+							'rm -rf /mnt/boot/system-proxy', this.link,
+						);
 
 						test.comment(`Manually restarting services...`);
-						await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								'systemctl restart balena-proxy-config.service redsocks.service',
-								this.context.get().link,
-							);
+						await this.worker.executeCommandInHostOS(
+							'systemctl restart balena-proxy-config.service redsocks.service',
+							this.link,
+						);
 					},
 				};
 			}),
