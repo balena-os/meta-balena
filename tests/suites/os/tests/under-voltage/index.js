bbdde584ec08e4197d9e317f48e7077314278640
Alexandru Costache <alexandru@balena.io>
2022-02-09 15:20:52 +0200

    suites/os: Add under-voltage test
    We add this this check since we discovered
    that with newer boot firmware the Pi4 will
    not reboot when under-voltage is reported.
    
    We leave this check to be performed on all
    devices to avoid missing any RPi derivate
    devices that may be added in the future,
    especilly since it's just a kernel log
    check.
    
    We also don't mark the test as failed in
    this case, because there may be devices that
    continue to work in the under-powered state and we
    don't want to block or delay development if the DUT
    is remote and cannot be checked or replaced easily.
    
    This check provides an indication when particular tests
    fail, like for instance if the device doesn't come up
    after a reboot or a hostOS update.
    
    Signed-off-by: Alexandru Costache <alexandru@balena.io>

---
+++ tests/suites/os/tests/under-voltage/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+	title: 'Under-voltage test',
+	run: async function(test) {
+		test.comment(`Checking for under-voltage reports in kernel logs...`);
+		let result = '';
+		result = await this.context
+			.get()
+			.worker.executeCommandInHostOS(
+				`dmesg | grep -q "Under-voltage detected" ; echo $?`,
+				this.context.get().link,
+			);
+
+		if (result.includes('0')) {
+			test.comment(`not ok! - Under-voltage detected on device, please check power source and cable!`);
+		} else {
+			test.comment(`ok - No under-voltage reports in the kernel logs`);
+		}
+
+		test.is(true, true, 'Under-voltage check completed');
+	},
+};
