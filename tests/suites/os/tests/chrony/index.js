ec7dcabd2e47ae9f7641388296c5c3c6e668c8bf
Kyle Harding <kyle@balena.io>
2022-06-15 09:00:53 -0400

    tests: os: chrony: Avoid conflicts with supervisor firewall
    Flushing the iptables can leave the engine and supervisor
    in a bad state. Instead just add and delete the NTP rules.
    
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- tests/suites/os/tests/chrony/index.js
+++ tests/suites/os/tests/chrony/index.js
@@ -1,3 +1,46 @@
+
+const blockNTP = async (that, test) => {
+	test.comment('blocking NTP traffic on port 123...');
+	// when supervisor starts up it flushes the INPUT table
+	// so wait for supervisor to be active before stopping it
+	return that.systemd.waitForServiceState('balena-supervisor.service', 'active', that.link)
+	.then(async () => {
+		return that.worker.executeCommandInHostOS(
+			[
+				`systemctl stop balena-supervisor`,
+				`&&`,
+				`journalctl --rotate`,
+				`&&`,
+				`journalctl --vacuum-time=1s`,
+				`&&`,
+				// make sure these rules take priority in the respective tables
+				`/usr/sbin/iptables -I INPUT 1 -p udp --destination-port 123 -j DROP`,
+				`&&`,
+				`/usr/sbin/iptables -I OUTPUT 1 -p udp --destination-port 123 -j DROP`,
+				`&&`,
+				`systemctl restart chronyd.service`
+			].join(' '),
+			that.link,
+		);
+	}).then(async () => {
+		return that.systemd.waitForServiceState('chronyd.service', 'active', that.link);
+	});
+}
+
+const restoreNTP = async (that, test) => {
+	test.comment('restoring NTP traffic on port 123...');
+	return that.worker.executeCommandInHostOS(
+		[
+			`/usr/sbin/iptables -D INPUT -p udp --destination-port 123 -j DROP`,
+			`&&`,
+			`/usr/sbin/iptables -D OUTPUT -p udp --destination-port 123 -j DROP`,
+			`&&`,
+			`systemctl restart balena-supervisor`,
+		].join(' '),
+		that.link,
+	);
+}
+
 module.exports = {
 	title: 'chrony tests',
 	tests: [
@@ -57,73 +100,54 @@ module.exports = {
 		{
 			title: 'Offline sources test',
 			run: async function(test) {
-				await this.worker.executeCommandInHostOS(
-						[
-						`iptables-save > /var/run/iptables.back`,
-						`&&`,
-						`journalctl --rotate`,
-						`&&`,
-						`journalctl --vacuum-time=1s`
-						].join(' '),
-						this.link,
+				return blockNTP(this, test)
+				.then(() => {
+					return test.resolves(
+						this.utils.waitUntil(async () => {
+							return this.worker.executeCommandInHostOS(
+									`journalctl -u chronyd.service | grep -q "No online NTP sources - forcing poll" >/dev/null 2>&1 ; echo $?`,
+									this.link,
+								)
+								.then( (output) => {
+									return Promise.resolve(output === '0')
+								})
+								.catch((err) => {
+									Promise.reject(err)
+								})
+						}, false, 5 * 60, 1000),
+						'Should force NTP poll when sources become offline'
 					);
-				await this.worker.executeCommandInHostOS(
-						[
-						`/usr/sbin/iptables -F`,
-						`&&`, `/usr/sbin/iptables -A INPUT -p udp --destination-port 123 -j DROP`,
-						`&&`, `/usr/sbin/iptables -A OUTPUT -p udp --destination-port 123 -j DROP`,
-						`&&`, `systemctl restart chronyd.service`
-						].join(' '),
-						this.link,
-					);
-				await this.systemd.waitForServiceState('chronyd.service', 'active', this.link)
-				await this.utils.waitUntil(async () => {
-					return this.worker.executeCommandInHostOS(
-							`journalctl -u chronyd.service | grep -q "No online NTP sources - forcing poll" >/dev/null 2>&1 && echo "pass"`,
-							this.link,
-						)
-						.then( (output) => {
-							test.is(output === 'pass', true, 'Should force NTP poll when sources become offline');
-							return Promise.resolve(output === 'pass')
-						})
-						.catch((err) => {
-							Promise.reject(err)
-						})
-				}, false, 2 * 60, 1000)
+				}).then(() => {
+					return restoreNTP(this, test);
+				});
 			},
 		},
 		{
 			title: 'System time skew test',
 			run: async function(test) {
-				await this.worker.executeCommandInHostOS(
-						[
-						`journalctl --rotate`,
-						`&&`,
-						`journalctl --vacuum-time=1s`,
-						`&&`,
-						`date --set="-2min"`,
-						].join(' '),
-						this.link,
+				return blockNTP(this, test)
+				.then(() => {
+					return this.worker.executeCommandInHostOS(`date --set="-2min"`, this.link);
+				}).then(() => {
+					return test.resolves(
+						this.utils.waitUntil(async () => {
+							return this.worker.executeCommandInHostOS(
+									`journalctl -u chronyd.service | grep -q "NTP time lost synchronization - restarting chronyd" >/dev/null 2>&1 ; echo $?`,
+									this.link,
+								)
+								.then( (output) => {
+									return Promise.resolve(output === '0')
+								})
+								.catch((err) => {
+									Promise.reject(err)
+								})
+						}, false, 5 * 60, 1000),
+						'Should restart chronyd when system time skew detected'
 					);
-				await this.systemd.waitForServiceState('chronyd.service', 'active', this.link)
-				await this.utils.waitUntil(async () => {
-					return this.worker.executeCommandInHostOS(
-							`journalctl -u chronyd.service | grep -q "NTP time lost synchronization - restarting chronyd" >/dev/null 2>&1 && echo "pass"`,
-							this.link,
-						)
-						.then( (output) => {
-							test.is(output === 'pass', true, 'Should restart chronyd when system time skew detected');
-							return Promise.resolve(output === 'pass')
-						})
-						.catch((err) => {
-							Promise.reject(err)
-						})
-				}, false, 2 * 60, 1000)
-				await this.worker.executeCommandInHostOS(
-						`iptables-restore < /var/run/iptables.back && rm -f /var/run/iptables.back`,
-						this.link,
-					);
-			},
+				}).then(() => {
+					return restoreNTP(this, test);
+				});
+			}
 		},
 	],
 };
