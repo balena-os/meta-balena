266b606631b8dc53d0eeee04b55868f5fc495951
Joseph Kogut <joseph@balena.io>
2022-09-28 11:28:03 -0700

    tests: preload: add backoff to retries
    Replace use of utils.waitUntil() w/ bluebird-retry using backoff.
    
    Change-type: patch
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- tests/suites/cloud/tests/preload/index.js
+++ tests/suites/cloud/tests/preload/index.js
@@ -1,38 +1,50 @@
 "use strict";
 
 const request = require('request-promise');
+const retry = require('bluebird-retry');
 
 module.exports = {
   title: "Image preload test",
   run: async function (test) {
+    const retryOptions = { backoff: 1.5, max_tries: 15 }; // appx 10m
 
     // if test is being done on physical DUT via the testbot, check that the preloaded application is working
     if(this.workerContract.workerType !== `qemu`){
       // we should be able to see the app starting.
       const ip = await this.worker.ip(this.link);
-      
+
       // create tunnel to DUT port 80
       console.log(`Creating tunnel to DUT port 80...`)
       await this.worker.createTunneltoDUT(this.link, 80, 8899);
-      await this.utils.waitUntil(async () => {
-        console.log(`Checking preloaded app is running... `)
-        let page = await request({
-          method: 'GET',
-          uri: `http://${ip}:80`,
-        })
-        return page.includes("Welcome to balena!"); 
-      }, false);
+      test.resolves(
+        retry(
+          async () => {
+            console.log(`Checking preloaded app is running... `)
+            let page = await request({
+              method: 'GET',
+              uri: `http://${ip}:80`,
+            })
+            if (!page.includes("Welcome to balena!")) {
+              throw new Error('Preloaded app did not return the expected response');
+            }
+          },
+          retryOptions
+        ),
+        `Web page should be exposed on port 80 of DUT`
+      );
 
-      test.ok(true, `Web page should be exposed on port 80 of DUT`)
       // When we confirm the app has started, then re-enable internet access to DUT
       await this.worker.executeCommandInWorker('sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"');
     }
 
     // make sure DUT is online
     console.log(`Waiting for DUT to be online in dashboard`)
-    await this.utils.waitUntil(() => {
-      return this.cloud.balena.models.device.isOnline(this.balena.uuid);
-    }, false, 60, 5 * 1000);
+    retry(
+      () => {
+        const online = this.cloud.balena.models.device.isOnline(this.balena.uuid);
+        if (!online) throw new Error('Device is not online');
+      }, retryOptions
+    );
 
     // wait until the service is running
     await this.cloud.waitUntilServicesRunning(
@@ -59,11 +71,16 @@ module.exports = {
       this.balena.uuid
     );
 
-    await this.utils.waitUntil(async () => {
-      console.log('Waiting for device to be running latest release...');
-      return this.cloud.balena.models.device.isTrackingApplicationRelease(
-        this.balena.uuid
-      );
-    }, false, 180, 5 * 1000);
+    await retry(
+      async () => {
+        console.log('Waiting for device to be running latest release...');
+        const latest = this.cloud.balena.models.device.isTrackingApplicationRelease(
+          this.balena.uuid
+        );
+
+        if (!latest) throw new Error('Device is not running the latest release');
+      },
+      retryOptions
+    );
   },
 };
