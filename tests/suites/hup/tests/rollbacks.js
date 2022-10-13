51f558450d60776470f92341789e14eecc217a65
rcooke-warwick <ryan@balena.io>
2022-10-13 07:43:29 +0000

    tests: hup: reduce num. flashes and  hostapp sends
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- tests/suites/hup/tests/rollbacks.js
+++ tests/suites/hup/tests/rollbacks.js
@@ -6,14 +6,16 @@
 
 'use strict';
 
+
 module.exports = {
-	title: 'Rollback health tests',
+	title: 'Rollback  tests',
+    run: async function (test) {
+        await this.hup.initDUT(this, test, this.link);
+    },
 	tests: [
-		{
+        {
 			title: 'Broken balena-engine',
 			run: async function(test) {
-				await this.hup.initDUT(this, test, this.link);
-
 				const origVersion = await this.worker.getOSVersion(this.link);
 
 				const activePartition = await this.worker.executeCommandInHostOS(
@@ -25,7 +27,6 @@ module.exports = {
 					this,
 					test,
 					'local',
-					this.hupOs.image.path,
 					this.link,
 				);
 
@@ -114,8 +115,6 @@ module.exports = {
 		{
 			title: 'Broken VPN',
 			run: async function(test) {
-				await this.hup.initDUT(this, test, this.link);
-
 				const origVersion = await this.worker.getOSVersion(this.link);
 
 				const activePartition = await this.worker.executeCommandInHostOS(
@@ -127,7 +126,6 @@ module.exports = {
 					this,
 					test,
 					'local',
-					this.hupOs.image.path,
 					this.link,
 				);
 
@@ -232,5 +230,105 @@ module.exports = {
 				);
 			},
 		},
+		{
+			title: 'Rollback altboot (broken init) test',
+			run: async function(test) {		
+				const origVersion = await this.worker.getOSVersion(this.link);
+		
+				const activePartition = await this.worker.executeCommandInHostOS(
+					`findmnt --noheadings --canonicalize --output SOURCE /mnt/sysroot/active`,
+					this.link,
+				);
+		
+				await this.hup.doHUP(
+					this,
+					test,
+					'local',
+					this.link,
+				);
+		
+				test.is(
+					await this.worker.executeCommandInHostOS(
+						`rm /mnt/sysroot/inactive/current/boot/init ; echo $?`,
+						this.link,
+					),
+					'0',
+					'Should delete mobynit to trigger rollback-altboot'
+				);
+		
+				await this.worker.rebootDut(this.link);
+		
+				await test.resolves(
+					this.utils.waitUntil(async () => {
+						return this.worker.executeCommandInHostOS(
+							`findmnt --noheadings --canonicalize --output SOURCE /mnt/sysroot/active`,
+							this.link,
+						).then(out => {
+							return out === activePartition;
+						})
+					}, false, 5 * 60, 1000),	// 5 min
+					'Should have rolled back to the original root partition'
+				);
+		
+				// 0 means file exists, 1 means file does not exist
+				await test.resolves(
+					this.utils.waitUntil(async () => {
+						return this.worker.executeCommandInHostOS(
+							`test -f /mnt/state/rollback-health-breadcrumb ; echo $?`,
+							this.link,
+						).then(out => {
+							return out === '1';
+						})
+					}, false, 5 * 60, 1000),	// 5 min
+					'Should not have rollback-health-breadcrumb in the state partition'
+				);
+		
+				// 0 means file exists, 1 means file does not exist
+				test.is(
+					await this.worker.executeCommandInHostOS(
+						`test -f /mnt/state/rollback-altboot-breadcrumb ; echo $?`,
+						this.link,
+					),
+					'1',
+					'Should not have rollback-altboot-breadcrumb in the state partition',
+				);
+		
+				// 0 means file exists, 1 means file does not exist
+				test.is(
+					await this.worker.executeCommandInHostOS(
+						`test -f /mnt/state/rollback-altboot-triggered ; echo $?`,
+						this.link,
+					),
+					'0',
+					'Should have rollback-altboot-triggered in the state partition',
+				);
+		
+				// 0 means file exists, 1 means file does not exist
+				test.is(
+					await this.worker.executeCommandInHostOS(
+						`test -f /mnt/state/rollback-health-triggered ; echo $?`,
+						this.link,
+					),
+					'1',
+					'Should not have rollback-health-triggered in the state partition',
+				);
+		
+				// 0 means file exists, 1 means file does not exist
+				test.is(
+					await this.worker.executeCommandInHostOS(
+						`test -f /mnt/state/rollback-health-failed ; echo $?`,
+						this.link,
+					),
+					'1',
+					'Should not have rollback-health-failed in the state partition',
+				);
+		
+				test.is(
+					await this.worker.getOSVersion(this.link),
+					origVersion,
+					`Should have rolled back to the original OS version`,
+				);
+			},
+		}
 	],
 };
