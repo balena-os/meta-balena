5534839614a5221068d25fb703658b780d6c7463
Alex Gonzalez <alexg@balena.io>
2022-11-12 12:27:34 +0100

    ssh-auth: setConfig: run synchronously
    Most tests expects a specific device configuration so having the setting
    run asynchronously leads to unexpected failures.
    
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- tests/suites/cloud/tests/ssh-auth/index.js
+++ tests/suites/cloud/tests/ssh-auth/index.js
@@ -24,7 +24,7 @@ const sshPath = join(homedir(), "test_id");
 
 const setConfig = async (test, that, target, key, value) => {
 
-	test.test(`Update or delete ${key} in config.json`, t =>
+	return test.test(`Update or delete ${key} in config.json`, t =>
 		t.resolves(
 			that.waitForServiceState(
 				'config-json.service',
