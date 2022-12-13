1356c4f244be8b67fac9da9e15d2dee8c3816611
rcooke-warwick <ryan@balena.io>
2022-12-13 17:08:43 +0000

    tests: hup: clean up inactive partition pre hup
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- tests/suites/hup/suite.js
+++ tests/suites/hup/suite.js
@@ -81,10 +81,21 @@ const enableSerialConsole = async (imagePath) => {
 const doHUP = async (that, test, mode, target) => {
 	const balenaHostTmpPath = "/mnt/sysroot/inactive/balena/tmp";
 	const hupLoadTmp = "/mnt/data/resin-data/tmp";
-	const inactiveStorage = "/mnt/sysroot/inactive/docker";
+	const inactiveStorage = "/mnt/sysroot/inactive/balena";
 
 	await that.worker.executeCommandInHostOS(
+		`systemctl stop balena-host`,
+		target,
+	);
+
+	test.comment(`Cleaning up inactive partition`);
+	await that.worker.executeCommandInHostOS(
 		`find "${inactiveStorage}" -mindepth 1 -maxdepth 1 -exec rm -r "{}" \\; || true`,
+		target,
+	)
+
+	await that.worker.executeCommandInHostOS(
+		`systemctl start balena-host`,
 		target,
 	);
 
