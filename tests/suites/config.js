20b8d57d6a8760cd012554b73d5ea0c3281a5331
rcooke-warwick <ryan@balena.io>
2023-11-01 12:02:07 +0000

    only run cloud suite
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- tests/suites/config.js
+++ tests/suites/config.js
@@ -1,68 +1,7 @@
 module.exports = [
 {
 	deviceType: process.env.DEVICE_TYPE,
-	suite: `${__dirname}/../suites/hup`,
-	config: {
-		networkWired: false,
-		networkWireless: false,
-		downloadVersion: 'latest',
-		balenaApiKey: process.env.BALENACLOUD_API_KEY,
-		balenaApiUrl: process.env.BALENACLOUD_API_URL,
-		organization: process.env.BALENACLOUD_ORG,
-		sshConfig: {
-			host: process.env.BALENACLOUD_SSH_URL,
-			port: process.env.BALENACLOUD_SSH_PORT,
-		}
-	},
-	image: `${__dirname}/balena-image.docker`,
-	debug: {
-		// Exit the ongoing test suite if a test fails
-		failFast: true,
-		// Exit the ongoing test run if a test fails
-		globalFailFast: false,
-		// Persist downloadeded artifacts
-		preserveDownloads: false,
-		// Mark unstable tests to be skipped
-		unstable: ['']
-	},
-	workers: process.env.WORKER_TYPE === 'qemu' ? ['http://worker'] : {
-		balenaApplication: process.env.BALENACLOUD_APP_NAME,
-		apiKey: process.env.BALENACLOUD_API_KEY,
-	},
-},
-{
-	deviceType: process.env.DEVICE_TYPE,
 	suite: `${__dirname}/../suites/cloud`,
-	config: {
-		networkWired: false,
-		networkWireless: false,
-		balenaApiKey: process.env.BALENACLOUD_API_KEY,
-		balenaApiUrl: process.env.BALENACLOUD_API_URL,
-		organization: process.env.BALENACLOUD_ORG,
-		sshConfig: {
-			host: process.env.BALENACLOUD_SSH_URL,
-			port: process.env.BALENACLOUD_SSH_PORT,
-		}
-	},
-	image: `${__dirname}/balena.img.gz`,
-	debug: {
-		// Exit the ongoing test suite if a test fails
-		failFast: true,
-		// Exit the ongoing test run if a test fails
-		globalFailFast: false,
-		// Persist downloadeded artifacts
-		preserveDownloads: false,
-		// Mark unstable tests to be skipped
-		unstable: ['']
-	},
-	workers: process.env.WORKER_TYPE === 'qemu' ? ['http://worker'] : {
-		balenaApplication: process.env.BALENACLOUD_APP_NAME,
-		apiKey: process.env.BALENACLOUD_API_KEY,
-	},
-},
-{
-	deviceType: process.env.DEVICE_TYPE,
-	suite: `${__dirname}/../suites/os`,
 	config: {
 		networkWired: false,
 		networkWireless: false,
