123e5476c4740abb29e0818d93449ca8da804f5c
Kyle Harding <kyle@balena.io>
2024-09-06 16:01:38 -0400

    tests:os: Avoid setting apiEndpoint for unmanaged tests
    When this config.json field has a value, the supervisor will treat the device as managed and will crash and start to boot loop.
    
    On faster device-types we don't seem to notice and the tests pass by chance, but on Pi Zero and similar we consistently see ECONNRESET when hitting local SV endpoints.
    
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- tests/suites/hup/suite.js
+++ tests/suites/hup/suite.js
@@ -327,7 +327,7 @@ module.exports = {
 			this.suite.options.balenaOS.network.wired = {
 				nat: true,
 			};
-		} 
+		}
 		else {
 			// device has wifi, use wifi hotspot to connect to DUT
 			delete this.suite.options.balenaOS.network.wired;
@@ -347,7 +347,7 @@ module.exports = {
 				psk: `${this.suite.options.id}_psk`,
 				nat: true,
 			};
-		} 
+		}
 		else {
 			// no wifi on DUT
 			delete this.suite.options.balenaOS.network.wireless;
@@ -378,13 +378,13 @@ module.exports = {
 		);
 
 		const keys = await this.utils.createSSHKey(this.sshKeyPath);
-		
+
 		// Authenticating balenaSDK
     await this.context
     .get()
     .sdk.balena.auth.loginWithToken(this.suite.options.balena.apiKey);
     this.log(`Logged in with ${await this.context.get().sdk.balena.auth.whoami()}'s account on ${this.suite.options.balena.apiUrl} using balenaSDK`);
-		
+
 		await this.sdk.balena.models.key.create(
 			this.sshKeyLabel,
 			keys.pubKey
@@ -417,7 +417,6 @@ module.exports = {
 						persistentLogging: true,
 						// Set local mode so we can perform local pushes of containers to the DUT
 						localMode: true,
-						apiEndpoint: 'https://api.balena-cloud.com',
 						developmentMode: true,
 						installer: {
 							secureboot: ['1', 'true'].includes(process.env.FLASHER_SECUREBOOT),
