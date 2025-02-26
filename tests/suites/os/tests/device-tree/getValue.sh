2f65f40490329f6e9ab9e32778564f821eb6fb4e
Florin Sarbu <florin@balena.io>
2025-02-26 22:40:54 +0000

    tests/device-tree: Rework test to not use the sysfs gpio interface
    The /sys/class/gpio interface is bound to be removed anytime so let's
    not use it anymore. Instead just make the test set the pin to high then
    check its value after reboot then set the pin to low and check its value
    again after the next reboot.
    
    Also, when checking the pin value in /sys/kernel/debug/gpio lets search
    for the gpio using the GPIO label instead of grepping for a gpio number
    because various kernel versions may offset the gpios with a base.
    
    Change-type: patch
    Signed-off-by: Florin Sarbu <florin@balena.io>

--- tests/suites/os/tests/device-tree/getValue.sh
+++ tests/suites/os/tests/device-tree/getValue.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /sys/kernel/debug/gpio | grep "gpio-4" -m 1 /sys/kernel/debug/gpio | awk '{print "\""$6"\""}'
+cat /sys/kernel/debug/gpio | grep "KEY_POWER" -m 1 /sys/kernel/debug/gpio | awk '{print "\""$6"\""}'
